59dc886d6d2f24535e1646b4d61d5f014db8e239
ShareeO <selena74354@yahoo.com>
2021-07-18 20:14:24 -0500

    created button and function to start quiz

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+// reference variables
+const startBtnEl = document.querySelector("#start-btn");
+
+//event handlers
+function startQuiz() {
+    console.log("handling start quiz");
+
+    //start timer
+console.log("start timer")
+
+    //show first question
+console.log("get first question")
+};
+
+// event listeners
+startBtnEl.addEventListener("click");
+
+
+
+
+
+
+
+
+
+
