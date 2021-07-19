45a70326e0411cf7c21e7aca3f7ecaac26e29f0e
ShareeO <selena74354@yahoo.com>
2021-07-18 20:32:12 -0500

    scoreEl goes down every second

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,26 @@
+// global variables
+let score = 10;
+let timerId;
+
 // reference variables
 const startBtnEl = document.querySelector("#start-btn");
+const scoreEl = document.querySelector("#score");
+
+scoreEl.textContent = score;
 
 //event handlers
 function startQuiz() {
     console.log("handling start quiz");
 
     //start timer
-console.log("start timer")
+    timerId = setInterval(function() {
+        score--;
+        scoreEl.textContent = score
+
+    }, 1000)
 
     //show first question
-console.log("get first question")
+    console.log("get first question");
 };
 
 // event listeners
